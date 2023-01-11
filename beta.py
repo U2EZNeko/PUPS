439fa7460b04733f91c9a2f465fe1c427861ac9b
U2EZNeko <nobody.cares@gmx.net>
2023-01-11 07:53:58 +0100

    Update beta.py

--- beta.py
+++ beta.py
@@ -1,3 +1,8 @@
+# Don't run this, it will probably delete your source playlists
+# Don't run this, it will probably delete your source playlists
+# Don't run this, it will probably delete your source playlists
+# Don't run this, it will probably delete your source playlists
+# Don't run this, it will probably delete your source playlists
 from plexapi.server import PlexServer
 
 def main(playlists):
@@ -9,12 +14,16 @@ def main(playlists):
 
 
 #Fill these values
-def main(playlists):
-    for item in playlists:
-        playlist_target = plexAdmin.playlist(item)
-        existing_playlist = plexAdmin.playlist(title=playlist_target.title, user=targetUser)
-        if existing_playlist:
-            existing_playlist.delete()
-            print('Playlist ' + playlist_target.title + ' already exists, it was deleted before copying. ')
-        playlist_target.copyToUser(targetUser)
-        print('Playlist ' + playlist_target.title + ' copied to user: ' + targetUser + ' successfully. ')
+if __name__ == "__main__":
+#   Server IP here, pref from internal network but should work externally too, didn't test.
+    baseurl = 'http://69.420.69.420:32400'
+#   Put Plex Token of Server Admin here
+    plexAdminToken = 'Put-Token-Here'
+#   Target Username, again, only Managed Users work. 
+    targetUser = 'TargetUserName'
+#   Add your playlist names here seperated with commas, use "these" quotes since ' is probably in one of ur playlist names.
+    list_of_playlists = ["Playlist1","Playlist2","etc"]
+    
+    plexAdmin = PlexServer(baseurl, plexAdminToken)
+    main(list_of_playlists)
+    print("Done")
